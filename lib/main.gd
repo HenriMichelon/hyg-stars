00626b4bb119a8f02a055eeb7be78c714d9a71fb
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-03-09 11:06:20 +0100

    added screenshot

--- lib/main.gd
+++ lib/main.gd
@@ -7,6 +7,6 @@ var stars_db:StarsDB
 
 func _ready():
 	stars_db = $Universe.stars_db
-	var star:Star = stars_db.get_star_proper("Sirius")
+	var star:Star = stars_db.get_star_proper("Sol")
 	player.position = star.position
 
